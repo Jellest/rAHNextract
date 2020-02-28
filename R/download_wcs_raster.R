13aa296e39953830c61b81d7ed3654c37fc15178
Jelle Stuurman <jellejstuurman@gmail.com>
2020-02-28 17:15:08 +0100

    improved function variables and wcs download

--- R/download_wcs_raster.R
+++ R/download_wcs_raster.R
@@ -9,13 +9,33 @@
 #'download_wcs_raster(name = "elevation", wcsUrl)
 #'@return .tif float32 file of BBOX area.
 #'
-download_wcs_raster <- function(wcsUrl, name = "elevation"){
+download_wcs_raster <- function(wcsUrl, name = "elevation", AHN = "AHN3", dem = "dsm"){
         if(!dir.exists("output")){
                 dir.create(paste("output"), showWarnings = FALSE)
         }
-        image_name <- paste0("output/", name, ".tif")
+
+        name_directory <- paste0("output/", name)
+        if(!dir.exists(name_directory)){
+                dir.create(name_directory, showWarnings = FALSE)
+        }
+
+
+        #ahn directory
+        ahn_directory <- paste(name_directory, AHN, sep="/")
+        if(!dir.exists(ahn_directory)){
+                dir.create(paste(name_directory, AHN, sep="/"), showWarnings = FALSE)
+        }
+
+        #working directory
+        working_directory <- paste(ahn_directory, dem, sep="/")
+        if(!dir.exists(working_directory)){
+                dir.create(paste(ahn_directory, dem, sep="/"), showWarnings = FALSE)
+        }
+
+        image_name <- paste0(working_directory, "/", name, ".tif")
+
         utils::download.file(wcsUrl, image_name, mode="wb")
-        print("Download raster image succeeded.")
+        print("Download raster image succeeded.");
         my_raster <- raster::raster(image_name)
         raster::NAvalue(my_raster) <- -32768.0
         #plot(my_raster, xlab="RD X", ylab="RD Y", main="Elevation (m)")
